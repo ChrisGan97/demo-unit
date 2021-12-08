e4fb43e7be625ba58f5f2b9eb393e3d835dc1019
Samroon Michael <samroonmichael@gmail.com>
2021-12-08 15:24:45 +0500

    I hope this commit will be blocked

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = function () {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
+                    Another Text
                 </a>
             </header>
         </div>
